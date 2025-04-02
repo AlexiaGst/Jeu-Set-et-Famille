20e635a2c5526285792c8015e5478b2d5a2ecac0
jadebiss <132221847+jadebiss@users.noreply.github.com>
2025-04-02 12:15:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,50 @@
+<DOCTYPE html>
+
+    <html>
+        <head>
+        <title> Jeu, Set et Famille </title>
+        <link rel="icon" href="images/logo.ico" type="image/x-icon">
+        <meta charset="utf-8"/>
+        <link rel="stylesheet" type="text/css" href="style/header_css.css">
+        <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,800&display=swap" rel="stylesheet">
+        </head>
+
+
+  <body>
+  
+	<!-- Header de la page -->
+
+    <header>
+        <a href="../index.php">
+        <img src="images/logo.png" alt="Jeu-Set-et-Famille logo" class="logo">
+        </a>
+        <input type="checkbox" id="nav_check" hidden>
+        <nav>
+            <ul>
+                <li>
+                    <a href="" > Page 1 </a>
+                </li>
+                <li>
+                    <a href=""> Page 2 </a>
+                </li>
+                <li>
+                    <a href=""> Page 3 </a>
+                </li>
+                <li>
+                    <a href=""> Page 4 </a>
+                </li>
+            </ul>
+        </nav>
+        <label for="nav_check" class="hamburger">
+            <div></div>
+            <div></div>
+            <div></div>
+        </label>
+    </header>
+
+
+
+
+
+
+  </html>
