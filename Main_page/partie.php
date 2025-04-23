7c817f41c9335f10ff6a5ee43aa1bcc51dc6493a
AlexiaGst <167088210+AlexiaGst@users.noreply.github.com>
2025-04-23 15:19:10 +0200

    Update partie.php

--- Main_page/partie.php
+++ Main_page/partie.php
@@ -1,7 +1,29 @@
-<?php
-include 'Login/bd_user.php';
-session_start();
-
+<?php 
+    include 'Login/bd_user.php';
+	session_start();
+	if (isset($_GET['id_partie'])) {
+		
+		
+		$id_partie = $_GET['id_partie'];
+		$id_joueur = $_SESSION['nom_utilisateur'];
+		
+		$sql = "SELECT nbr_joueurs FROM parties WHERE id_partie=$id_partie";
+		$res = mysqli_query($bdd, $sql);
+		$row = mysqli_fetch_assoc($res);
+		$nb_joueurs=$row['nbr_joueurs'];
+		$sql = "SELECT u.id_client, u.nom_utilisateur 
+				FROM utilisateurs u
+				JOIN jeu ON jeu.id_joueur = u.id_client
+				WHERE jeu.id_partie = $id_partie
+				ORDER BY jeu.id_joueur";
+				
+		$res = mysqli_query($bdd, $sql);
+		$joueurs = [];
+		while ($row = mysqli_fetch_assoc($res)) {
+			$joueurs[] = $row;
+		}
+	}
+			
 ?>
 
 
@@ -13,6 +35,10 @@ session_start();
     <link rel="icon" href="images/logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="style/game.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,800&display=swap" rel="stylesheet">
+    <script src="js/gestion_cartes.js" defer></script>
+	<script>
+		const monNom = <?php echo json_encode($_SESSION['nom_utilisateur']); ?>;
+	</script>
 </head>
 
 <body>
@@ -22,7 +48,7 @@ session_start();
 
 <div class="grid-layout">
   <div class="top-section">
-  <div class="player-info">
+  <div class="player-info" data-joueur="?">
   <div class="profile-timer">
     <svg class="progress-ring" width="80" height="80">
       <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
@@ -43,7 +69,7 @@ session_start();
   <div class="middle-section">
     <div class="box">
     <div class="two-player">
-    <div class="player-info">
+    <div class="player-info" data-joueur="?">
   <div class="profile-timer">
     <svg class="progress-ring" width="80" height="80">
       <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
@@ -61,7 +87,7 @@ session_start();
       </div>
       </div>
 
-      <div class="player-info">
+      <div class="player-info" data-joueur="?">
   <div class="profile-timer">
     <svg class="progress-ring" width="80" height="80">
       <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
@@ -104,7 +130,7 @@ session_start();
 
 <div class="box">
   <div class="two-player2">
-    <div class="player-info">
+    <div class="player-info" data-joueur="?">
   <div class="profile-timer">
     <svg class="progress-ring" width="80" height="80">
       <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
@@ -122,7 +148,7 @@ session_start();
       </div>
       </div>
 
-      <div class="player-info">
+      <div class="player-info" data-joueur="?">
   <div class="profile-timer">
     <svg class="progress-ring" width="80" height="80">
       <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
@@ -146,7 +172,7 @@ session_start();
   </div>
 
   <div class="bottom-section">
-  <div class="player-info">
+  <div class="player-info" data-joueur="?">
   <div class="profile-timer">
     <svg class="progress-ring" width="80" height="80">
       <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
