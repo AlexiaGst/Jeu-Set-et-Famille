8946cd5e873a02bada9d7f305a69f9255d086d31
AlexiaGst <167088210+AlexiaGst@users.noreply.github.com>
2025-05-07 15:45:42 +0200

    Update partie.php

--- Main_page/partie.php
+++ Main_page/partie.php
@@ -7,33 +7,16 @@ if (!isset($_SESSION['nom_utilisateur'])) {
     exit();
 }
 
-if (isset($_GET['id_partie'])) {
+if (isset($_GET['id_partie'])) {// Vérifie que la partie est bien lancée
 	$id_partie = $_GET['id_partie'];
 	$id_joueur = $_SESSION['nom_utilisateur'];
 
+	//récupère l'id du joueur
 	$sql="SELECT id_client FROM utilisateurs WHERE nom_utilisateur='$id_joueur'";
 	$res=mysqli_query($bdd, $sql);
 	$row=mysqli_fetch_assoc($res);
 	$id=$row['id_client'];
-	
-	
-	$sql = "SELECT nbr_joueurs FROM parties WHERE id_partie=$id_partie";
-	$res = mysqli_query($bdd, $sql);
-	$row = mysqli_fetch_assoc($res);
-	$nb_joueurs=$row['nbr_joueurs'];
-	$sql = "SELECT u.id_client, u.nom_utilisateur 
-			FROM utilisateurs u
-			JOIN jeu ON jeu.id_joueur = u.id_client
-			WHERE jeu.id_partie = $id_partie
-			ORDER BY jeu.id_joueur";
-
-	$res = mysqli_query($bdd, $sql);
-	$joueurs = [];
-	while ($row = mysqli_fetch_assoc($res)) {
-		$joueurs[] = $row;
-	}
 }
-
 
 // TABLEAU DES JOUEURS
 
@@ -53,15 +36,13 @@ $playerCount = intval($row_count['max_joueurs']);
     <meta charset="utf-8"/>
     <link rel="icon" href="images/logo.ico" type="image/x-icon">
     <link rel="stylesheet" href="style/game.css">
+	<link rel="stylesheet" href="style/fin_partie.css">
     <link href="https://fonts.googleapis.com/css?family=Poppins:400,500,600,800&display=swap" rel="stylesheet">
-    <script src="js/chat_bubble.js" defer></script>
 	<script src="js/pioche.js" defer></script>
     <script src="js/gestion_cartes.js" defer></script>
-    <script src="js/game_script.js" defer></script>
     <script>
 		const monNom = <?php echo json_encode($_SESSION['nom_utilisateur']); ?>;
     </script>
-     <link rel="stylesheet" href="style/fin_partie.css">
 </head>
 
 <body>
@@ -71,15 +52,19 @@ $playerCount = intval($row_count['max_joueurs']);
 			<div class="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
 	    </div>
 	</div>
+	<!--Conteneur des bulles d'informations-->
 	<div id="chat-container" class="chat-container"></div>
 
 
 
 <main>
     <a href='ongoing_games.php?retour=<?php echo $id_partie; ?>&quit=<?php echo $id; ?>' id='retour'>&lt; Retour</a>
+	
     <div id="wrapper_fin"></div>
 	
 	<div id="menu-cartes"></div>
+	
+	<div id="mes-cartes"></div>
 	
     <div class="grid-layout">
 
@@ -125,21 +110,21 @@ $playerCount = intval($row_count['max_joueurs']);
 						<div class="card2"></div>
 						<div class="card2"></div>
 						<div class="card2"></div>
-						
 						<div class="card2" id="pioche-count">
-							<span class="card-count">
-							</span>
+							<span class="card-count"></span>
 						</div>
 					</div>
                 </div>
+				
                 <div class="box">
 
                 </div>
             </div>
 
-        <?php elseif ($playerCount === 4): ?>
+        <?php elseif ($playerCount === 4 || $playerCount === 3): ?>
             <!-- Affichage pour 4 joueurs avec middle-section version 1 -->
             <div class="top-section">
+			<?php if ($playerCount === 4):?>
                 <div class="player-info" data-joueur="?" data-position="haut">
 					<div class="profile-timer">
 					  <svg class="progress-ring" width="80" height="80">
@@ -162,6 +147,7 @@ $playerCount = intval($row_count['max_joueurs']);
 					    <div class="card1"></div>
 				    </div>
 			    </div>
+				<?php endif;?>
             </div>
             <div class="middle-section">
 				<div class="box">
@@ -202,7 +188,7 @@ $playerCount = intval($row_count['max_joueurs']);
 						<div class="card2"></div>
 						<div class="card2"></div>
 						<div class="card2" id="pioche-count">
-						<span class="card-count"></span>
+							<span class="card-count"></span>
 						</div>
 					</div>
 				</div>
@@ -210,34 +196,34 @@ $playerCount = intval($row_count['max_joueurs']);
           <!-- -->
 
 				<div class="box right">
-					<div class="player-info" data-joueur="?" data-position="droite">
-					  
-					  <div class="profile-timer">
-						  <svg class="progress-ring" width="80" height="80">
-							<circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
-						  </svg>
-						  <img src="images/profil1.png" alt="Profil" class="profile-pic">
-						</div>
-						
-						<div class="player-text">
-							<span class="player-name">Joueur 1</span>
-							<div class="family-count">
-							  <span></span>
+						<div class="player-info" data-joueur="?" data-position="droite">
+						  
+						  <div class="profile-timer">
+							  <svg class="progress-ring" width="80" height="80">
+								<circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
+							  </svg>
+							  <img src="images/profil1.png" alt="Profil" class="profile-pic">
 							</div>
+							
+							<div class="player-text">
+								<span class="player-name">Joueur 1</span>
+								<div class="family-count">
+								  <span></span>
+								</div>
+							</div>
+							
+							<div class="cards1">
+							  <div class="card1"></div>
+							  <div class="card1"></div>
+							  <div class="card1"></div>
+							  <div class="card1"></div>
+						  </div>
+					  
 						</div>
-						
-						<div class="cards1">
-						  <div class="card1"></div>
-						  <div class="card1"></div>
-						  <div class="card1"></div>
-						  <div class="card1"></div>
-					  </div>
-				  
-					</div>
 				</div>
 			</div>
 
-        <?php elseif ($playerCount === 6): ?>
+        <?php elseif ($playerCount === 6 || $playerCount === 5): ?>
             <!-- Affichage pour 6 joueurs (middle-section complet) -->
             <div class="top-section">
                 <div class="player-info" data-joueur="?" data-position="haut">
@@ -362,28 +348,29 @@ $playerCount = intval($row_count['max_joueurs']);
 					    </div>
 
 					</div>
+					<?php if ($playerCount === 6):?>
+						<div class="player-info" data-joueur="?" data-position="droite_b">
 
-					<div class="player-info" data-joueur="?" data-position="droite_b">
-
-						<div class="profile-timer">
-							<svg class="progress-ring" width="80" height="80">
-							  <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
-							</svg>
-							<img src="images/profil1.png" alt="Profil" class="profile-pic">
+							<div class="profile-timer">
+								<svg class="progress-ring" width="80" height="80">
+								  <circle class="progress-ring__circle" stroke="green" stroke-width="5" fill="transparent" r="35" cx="40" cy="40"/>
+								</svg>
+								<img src="images/profil1.png" alt="Profil" class="profile-pic">
+							</div>
+							  <div class="player-text">
+									<span class="player-name">Joueur 1</span>
+									<div class="family-count">
+										<span></span>
+									</div>
+							  </div>
+							<div class="cards1">
+							  <div class="card1"></div>
+							  <div class="card1"></div>
+							  <div class="card1"></div>
+							  <div class="card1"></div>
+							</div>
 						</div>
-						  <div class="player-text">
-								<span class="player-name">Joueur 1</span>
-								<div class="family-count">
-									<span></span>
-								</div>
-						  </div>
-						<div class="cards1">
-						  <div class="card1"></div>
-						  <div class="card1"></div>
-						  <div class="card1"></div>
-						  <div class="card1"></div>
-					    </div>
-					</div>
+					<?php endif;?>
 				  </div>
 				</div>
 			</div>
@@ -404,19 +391,21 @@ $playerCount = intval($row_count['max_joueurs']);
 					  <span></span>
 					</div>
 				</div>
+				<div class="cards-wrapper">
+					<div class="cards">
+						<div class="card"></div>
+						<div class="card"></div>
+						<div class="card"></div>
+						<div class="card"></div>
+						<div class="card"></div>
+						<div class="card"></div>
+						<div class="card"></div>
+					</div>
+				</div>
+			</div>
 
-            <div class="cards">
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-                <div class="card"></div>
-            </div>
-        </div>
+		</div>
 
-    </div>
 </main>
 
 </body>
